95764a16f3424c2b8e659f822d0f50a36454ec5b
SyedAli310 <alizaidi310@gmail.com>
2021-12-18 22:22:26 +0530

    added edit functionality and major UI rewamps

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(document).ready(() => {
-  console.log("%cHello World", "color: green; font-size: 24px;");
+  console.log("%c-> Loading...", "color: royalblue; font-size: 14px;");
+  console.log(
+    "%cApp Initialized Successfully!",
+    "color: limegreen; font-size: 14px;"
+  );
+
+  const allModals = document.querySelectorAll(".modal");
 
   // function to generate random id for todo
   function generateKey(tokenLen, hyphen, hyphenPos) {
@@ -30,9 +36,10 @@ $(document).ready(() => {
 
   let all__todos = JSON.parse(localStorage.getItem("stored__todos")) || [];
   window.onload = renderTodos();
-  // Add todo
+
+  // add todo function
   function addTodo(todo, completed, priority) {
-    const todo_id = generateKey(8, true,4).toLowerCase();
+    const todo_id = generateKey(8, true, 4).toLowerCase();
     const dateAdded = new Date();
     if (!todo || todo.length == 0) {
       alert("Please enter a todo");
@@ -59,6 +66,7 @@ $(document).ready(() => {
     }
   }
 
+  // add todo handler
   $("#add-todo").on("submit", (e) => {
     e.preventDefault();
     const todo = $("#todo-input").val().trim();
@@ -67,51 +75,81 @@ $(document).ready(() => {
     addTodo(todo, completed, priority);
   });
 
-  // Delete todo
-  function deleteTodo(id, edited) {
+  // delete todo function
+  function deleteTodo(id) {
     all__todos = all__todos.filter((todo) => todo.id !== id);
     localStorage.setItem("stored__todos", JSON.stringify(all__todos));
-    if (!edited || edited == null || edited == undefined) {
-      renderTodos();
-    } else {
-      return;
-    }
+    renderTodos();
   }
 
-  // Edit todo
+  // edit todo function
   function editTodo(id) {
     const toBeEdited = all__todos.find((todo) => todo.id === id);
-    $("#todo-input").val(toBeEdited.todo);
-    $("#priority-input").val(toBeEdited.priority);
-    $("#completed-checkbox").prop("checked", toBeEdited.completed);
-    deleteTodo(id, true);
+    $("#edit-todo #edit-todo-input").val(toBeEdited.todo);
+    $("#edit-todo #edit-priority-input").val(toBeEdited.priority);
+    $("#edit-todo #edit-completed-checkbox").prop(
+      "checked",
+      toBeEdited.completed
+    );
+    $("#edit-todo-submit-btn").attr("data-id", id);
+    $(".edit-todo-modal").addClass("active");
   }
 
-  $('#sort-by-priority').on('click', () => {
-    renderTodos(true,false,false);
+  // edit todo submit handler
+  $("#edit-todo").on("submit", (e) => {
+    e.preventDefault();
+    const todo = $("#edit-todo-input").val().trim();
+    const completed = $("#edit-completed-checkbox").is(":checked");
+    const priority = $("#edit-priority-input").val();
+    const id = $("#edit-todo-submit-btn").attr("data-id");
+    const toBeEdited = all__todos.find((todo) => todo.id === id);
+    toBeEdited.todo = todo;
+    toBeEdited.completed = completed;
+    toBeEdited.priority = priority;
+    localStorage.setItem("stored__todos", JSON.stringify(all__todos));
+    renderTodos();
+    $(".edit-todo-modal").removeClass("active");
   });
-  $('#sort-by-completed').on('click', () => {
-    renderTodos(false,true,false);
+
+  $("#sort-by-priority").on("click", () => {
+    renderTodos(true, false, false);
   });
-  $('#sort-by-oldest').on('click', () => {
-    renderTodos(false,false,true);
+  $("#sort-by-completed").on("click", () => {
+    renderTodos(false, true, false);
   });
-  $('#sort-by-latest').on('click', () => {
-    renderTodos(false,false,false);
+  $("#sort-by-oldest").on("click", () => {
+    renderTodos(false, false, true);
+  });
+  $("#sort-by-latest").on("click", () => {
+    renderTodos(false, false, false);
   });
 
   // Render todos
   function renderTodos(byPriority, byCompleted, byDate) {
     $("#todo-list").html("");
+    const totalTodos = all__todos.length;
+    const completedTodos = all__todos.filter(
+      (todo) => todo.completed === true
+    ).length;
+    const pendingTodos = all__todos.filter(
+      (todo) => todo.completed === false
+    ).length;
+    $("#total-todos").text(totalTodos);
+    $("#total-completed").text(completedTodos);
+    $("#total-pending").text(pendingTodos);
     if (all__todos.length > 0) {
       if (byPriority) {
         all__todos.sort((a, b) => a.priority - b.priority);
       } else if (byCompleted) {
         all__todos.sort((a, b) => b.completed - a.completed);
       } else if (byDate) {
-        all__todos.sort((a, b) => new Date(a.dateAdded) - new Date(b.dateAdded));
+        all__todos.sort(
+          (a, b) => new Date(a.dateAdded) - new Date(b.dateAdded)
+        );
       } else {
-        all__todos.sort((a, b) => new Date(b.dateAdded) - new Date(a.dateAdded));
+        all__todos.sort(
+          (a, b) => new Date(b.dateAdded) - new Date(a.dateAdded)
+        );
       }
       all__todos.forEach((todo, index) => {
         const todo_li = document.createElement("li");
@@ -128,17 +166,19 @@ $(document).ready(() => {
                     <span class="todo-el-priority">Priority- ${
                       todo.priority
                     } |</span>
-                    <span style='color:${todo.completed ? "green" : "red"}'>${
-          todo.completed ? "Completed" : "Pending"
-        } </span>
+                    <span style='color:${
+                      todo.completed
+                        ? "var(--TEXT_SUCCESS)"
+                        : "var(--TEXT_DANGER)"
+                    }'>${todo.completed ? "Completed" : "Pending"} </span>
                     |
                     <button class='delete-todo-btn' todo-id='${
                       todo.id
-                    }' title='Delete ToDo'>Delete</button>
+                    }' title='Delete ToDo'><ion-icon name="trash-outline"></ion-icon></button>
                     |
                     <button class='edit-todo-btn' todo-id='${
                       todo.id
-                    }' title='Edit ToDo'>Edit</button>
+                    }' title='Edit ToDo'><ion-icon name="create-outline"></ion-icon></button>
                 </div>
                 <span>Last updated: ${new Date(
                   todo.dateAdded
@@ -147,8 +187,8 @@ $(document).ready(() => {
         ).toLocaleTimeString()}</span>
                 `;
         todo.completed
-          ? (todo_li.style.boxShadow = "3px 3px 0px green")
-          : (todo_li.style.boxShadow = "3px 3px 0px red");
+          ? (todo_li.style.boxShadow = "3px 3px 0px var(--TEXT_SUCCESS)")
+          : (todo_li.style.boxShadow = "3px 3px 0px var(--TEXT_DANGER)");
         $("#todo-list").append(todo_li);
 
         $(".delete-todo-btn").click((e) => {
@@ -166,4 +206,11 @@ $(document).ready(() => {
         `);
     }
   }
+
+  // close modal handler
+  $(".modal-close-btn").click(() => {
+    allModals.forEach((modal) => {
+      modal.classList.remove("active");
+    });
+  });
 });
